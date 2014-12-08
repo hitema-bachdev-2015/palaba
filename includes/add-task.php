616bcdf4df5071e159884b5df895de5774a55ba1
Kevin <contact@kevindesousa.fr>
2014-12-08 15:40:03 +0100

    [+] Includes

---
+++ includes/add-task.php
@@ -0,0 +1,27 @@
+<div id="blocAdd">
+	<div id="add">
+	<input type="text" id="inputTask" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Enter the new task here...">
+	<div id="blocParams">
+		<div id="selectCateg">
+			<select>
+				<option>Maison</option>
+				<option>Travail</option>
+				<option>Noel</option>
+			</select>
+		</div>
+		<div id="dateChoose">
+			<input type="date">
+		</div>
+		<div id="type">
+			<select>
+				<option>Au plus tôt</option>
+				<option>Avant de mourir</option>
+			</select>
+		</div>
+		</div>
+		<div id="divButton">
+			<div id="button"><span>OK</span></div> 
+		</div>
+		</div>
+	
+</div>
