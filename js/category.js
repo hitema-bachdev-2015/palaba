52d2c160c2c56da7c8fdef8dffe27803357e9899
Yannick <yannick.elusue@hotmail.fr>
2014-12-08 16:04:10 +0100

    Commit JS

---
+++ js/category.js
@@ -0,0 +1,5 @@
+var Category=function(params){
+	this.id=params.id;
+	this.name = params.name;
+	this.position = params.position;
+}
---
+++ js/task.js
@@ -0,0 +1,8 @@
+var Task=function(params){
+	this.id=params.id;
+	this.idCategory = params.idCategory;
+	this.content = params.content;
+	this.dateEnd = params.dateEnd;
+	this.endType = params.endType;
+	this.status = params.status;
+}
