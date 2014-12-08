7abade6e38af3d3c1f3d5c4d606b6573686f8abb
Yannick <yannick.elusue@hotmail.fr>
2014-12-08 19:05:47 +0100

    Future Task Object

--- js/task.js
+++ js/task.js
@@ -6,3 +6,12 @@ var Task=function(params){
 	this.endType = params.endType;
 	this.status = params.status;
 }
+Task.prototype.addTask=function(params){
+	//Mise de l'id de l'element+1 à ajouter, dans une variable
+	//Ajout de l'élément node li avant l'élément ayant la valeur de l'id element+1
+	//var li = document.createElement("li");        // Create a <button> element
+	//var content = document.createTextNode("CLICK ME");       // Create a text node
+	//li.appendChild(content);                                // Append the text to <button>
+	//var insertElement = parentElement.insertBefore(li, refElement); //append before refElement
+	//document.body.appendChild(li);                    // Append <button> to <body>
+}
