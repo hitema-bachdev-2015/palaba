d81a85d981ad668e9d508b7ff46b9ac591dc227f
anne-sop <anne-sophie.bach@outlook.com>
2014-12-08 16:17:12 +0100

    set and get fill

--- classes/Task.php
+++ classes/Task.php
@@ -13,34 +13,34 @@ class Task {
 	}
 
 	public setContent($c){
-
+		$this->content = $c;
 	}
 
 	public setDateEnd($de){
-
+		$this->date_end = $de;
 	}
 
 	public setEndType($et){
-
+		$this->end_type = $et;
 	}
 
 	public setStatus($s){
-
+		$this->status = $s;
 	}
 	public getContent(){
-
+		return $this->content;
 	}
 
 	public getDateEnd(){
-
+		return $this->date_end;
 	}
 
 	public getEndType(){
-
+		return $this->end_type;
 	}
 
 	public getStatus(){
-
+		return $this->status;
 	}
 	
 	public function update($cont, $dateEnd, $endType){
