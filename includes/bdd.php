a0341d9db0bb337cf71d5ee5d67f6ea8b1a42afd
anne-sop <anne-sophie.bach@outlook.com>
2014-12-08 16:55:50 +0100

    Bdd connect

--- includes/bdd.php
+++ includes/bdd.php
@@ -2,3 +2,9 @@
 include "config.php";
 
 //new PDO("mysql:host...", MYSQL_USER, MYSQL_PASS);
+$dbh = new PDO('mysql:host=localhost;dbname=MYSQL_DBNAME', 'MYSQL_USER', 'MYSQL_PASS', array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO:: FETCH_ASSOC,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+        ));
+
