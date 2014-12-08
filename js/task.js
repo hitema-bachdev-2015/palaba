472c6fdb76cc9253481bcd89330f0ea92f81149f
Yannick <yannick.elusue@hotmail.fr>
2014-12-08 19:16:55 +0100

    add addTask

--- js/task.js
+++ js/task.js
@@ -9,9 +9,7 @@ var Task=function(params){
 Task.prototype.addTask=function(params){
 	//Mise de l'id de l'element+1 à ajouter, dans une variable
 	//Ajout de l'élément node li avant l'élément ayant la valeur de l'id element+1
-	//var li = document.createElement("li");        // Create a <button> element
+	//var li = document.createElement("li");        // Create a <li> element
 	//var content = document.createTextNode("CLICK ME");       // Create a text node
-	//li.appendChild(content);                                // Append the text to <button>
+	//li.appendChild(content);                                // Append the text to <li>
 	//var insertElement = parentElement.insertBefore(li, refElement); //append before refElement
-	//document.body.appendChild(li);                    // Append <button> to <body>
-}
