c66827b579ddac6d6140bbba18a68f102cff52cd
Kevin <contact@kevindesousa.fr>
2014-12-08 15:28:00 +0100

    [+] Add Task Classes

---
+++ classes/Task.php
@@ -0,0 +1,53 @@
+<?php
+class Task {
+
+	protected $id;
+	private $id_category;
+	private $content;
+	private $date_end;
+	private $end_type;
+	private $status;
+
+	function __construct($id) {
+		//Hydratation
+	}
+
+	public setContent($c){
+
+	}
+
+	public setDateEnd($de){
+
+	}
+
+	public setEndType($et){
+
+	}
+
+	public setStatus($s){
+
+	}
+	public getContent(){
+
+	}
+
+	public getDateEnd(){
+
+	}
+
+	public getEndType(){
+
+	}
+
+	public getStatus(){
+
+	}
+	
+	public function update($cont, $dateEnd, $endType){
+
+	}
+
+	public function delete(){
+
+	}
+}
