6a5d94f9d1b63345826d4c7dc19de729eea86cab
Kevin <contact@kevindesousa.fr>
2014-12-08 20:48:45 +0100

    [*] Fix : Indentation

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php
-	if(file_exists('/wamp/www/PaLaBa/defines.inc.php'))
-	{
-		require_once('/wamp/www/PaLaBa/defines.inc.php');
-	}
-	elseif(file_exists('/Applications/MAMP/htdocs/PaLaBa/defines.inc.php')){
-		require_once('/Applications/MAMP/htdocs/PaLaBa/defines.inc.php');
-	}
-	else
-	{
-		define('_DB_HOST_', 'localhost');
-		define('_DB_NAME_', 'todolist');
-		define('_DB_USERNAME_', 'root');
-		define('_DB_PASSWORD_', '');
-	}
+if(file_exists('/wamp/www/PaLaBa/defines.inc.php'))
+{
+	require_once('/wamp/www/PaLaBa/defines.inc.php');
+}
+elseif(file_exists('/Applications/MAMP/htdocs/PaLaBa/defines.inc.php'))
+{
+	require_once('/Applications/MAMP/htdocs/PaLaBa/defines.inc.php');
+}
+else
+{
+	define('_DB_HOST_', 'localhost');
+	define('_DB_NAME_', 'todolist');
+	define('_DB_USERNAME_', 'root');
+	define('_DB_PASSWORD_', '');
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
 	<meta charset="utf-8">
-	
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
 	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
@@ -28,12 +28,12 @@
 </head>
 <body>
 
-<?php include('includes/add-task.php'); ?>
+	<?php include('includes/add-task.php'); ?>
 
-<?php include('includes/content.php'); ?>
+	<?php include('includes/content.php'); ?>
 
-<?php include('includes/add-category.php'); ?>
+	<?php include('includes/add-category.php'); ?>
 
-<?php include('includes/footer.php'); ?>
+	<?php include('includes/footer.php'); ?>
 </body>
 </html>
