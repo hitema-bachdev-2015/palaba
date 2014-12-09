806f1dede7885929b6bdf27b1a94fdc09b63925f
Kevin Martin <kevin.martin@quatdev.fr>
2014-12-09 15:24:02 +0100

    Category move update

--- includes/content.php
+++ includes/content.php
@@ -1,19 +1,2 @@
-<ul id="list-category">
-	<li class="title-category" data-id="1">
-		<ul>
-			<li data-id="1" >Acheter une baguette</li>
-		</ul>
-	</li>
-</ul>
-<ul id="list-category">
-	<li class="title-category" data-id="2">
-		<header>Noël</header>
-		<article>Acheter une montre</article>
-	</li>
-</ul>
-<ul id="list-category">
-	<li class="title-category" data-id="3">
-		<header>Maison</header>
-		<article>Conclure le dossier X5</article>
-	</li>
+<ul id="sortable">
 </ul>
