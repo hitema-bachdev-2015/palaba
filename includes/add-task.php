28dc17096982dddbb7d8785d9b45b9bc26e27d31
Antoine92 <antoine.hagues@gmail.com>
2014-12-09 10:31:31 +0100

    Ajout de l'ajax add-task dans script.js

--- includes/add-task.php
+++ includes/add-task.php
@@ -15,26 +15,6 @@
 		<input type="submit" name="submit_add-task" value="OK"/>
 	</form>
 	<script type="text/javascript">
-		$("#add-task form input[type='submit']").on("click",function(event){
-			event.preventDefault();
-			var Content_Task=$("#add-task form input[name='content-task']").val();
-			var Category_Task=$("#add-task form select[name='id-category-task']").val();
-			var Date_Picker=$("#datepicker").val();
-			var End_Type_Task=$("#add-task form select[name='end_type-task']").val();
-			$.ajax({
-				url : 'ajax/script-addtask.php',
-				type : 'POST',
-				data: {
-						CONTENT:Content_Task,
-						CATEGORY:Category_Task,
-						DATE:Date_Picker,
-						END_TYPE_TASK:End_Type_Task,
-					},
-       			success : function(data){ // code_html contient le HTML renvoyé
-       			alert(data);
-       			}
-   			});
-			//alert(Content_Task+" "+Category_Task+" "+Date_Picker+" "+End_Type_Task);
-		});
+		
 	</script>
 </header>
