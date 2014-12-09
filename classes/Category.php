11b1d6c2954b54814623c6f862df2f80386c79f9
anne-sop <anne-sophie.bach@outlook.com>
2014-12-09 13:35:54 +0100

    fix errror constructeur category

--- classes/Category.php
+++ classes/Category.php
@@ -2,29 +2,19 @@
 
 class Category {
 	protected $id;
-	private $name;
 
-	function __construct($id = null, $name) {
+
+	function __construct($id = null) {
 		$this->id = $id;
-		$this->name = $name;
 	}
-
 
 	public function setId($id){
 	 	$this->id = $id;
 	}
 
-	public function setName($name){
-		$this->name = $name;
-	}
-
 
 	public function getId(){
 		return $this->id;
-	}
-
-	public function getName(){
-		return $this->name;
 	}
 
 
