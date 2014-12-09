04854f414a024b048d290029280c1479cb38cbe0
ttclem <tt.clem@free.fr>
2014-12-09 11:04:37 +0100

    modif sur l'index partie ajout d'une tâche

--- includes/add-task.php
+++ includes/add-task.php
@@ -1,17 +1,19 @@
 <header id="add-task">
 	<form action="#" method="get">
-			<input type="text" name="content-task" placeholder="Enter the new task here...">
+			<input id="cont-task" type="text" name="content-task" placeholder="Enter the new task here..."> 
+		<div id="forKeyup">
 			<select name="id-category-task">
 				<option value="0">Maison</option>
 				<option value="1">Travail</option>
 				<option value="2">Noel</option>
 			</select>
 			<input type="text" id="datepicker" name="date_end-task" placeholder="Choissiez la date...">
-			<select name="end_type-task">
+			<select name="end_type-task" id="end_select">
 				<option value="1">Au plus tôt</option>
 				<option value="2">Date précise</option>
 				<option value="3">Avant de mourir</option>
 			</select>
 			<input type="submit" name="submit_add-task" value="OK"/>
+		</div>
 	</form>
 </header>
--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 //datepicker
 $(document).ready(function() {
-  
-    $("#datepicker").datepicker();
-  
+  	$("#datepicker").datepicker();
+  	$("#datepicker").hide();
+  	$("#forKeyup").hide();
+
+	$("select#end_select").change(function(){
+		var selected = $("#end_select option:selected").val();
+		if(selected == 2){
+			$("#datepicker").show();
+		}else if(selected != 2){
+			$("#datepicker").hide();
+		}
+	});
+
+	$("#cont-task").keyup(function(){
+		var valText = $("#cont-task").val().trim();
+			if(valText != null || valText != ''){
+				$("#forKeyup").show();
+			}
+			if(valText == null || valText == ''){
+				$("#forKeyup").hide();
+			}
+	});
 });
+
