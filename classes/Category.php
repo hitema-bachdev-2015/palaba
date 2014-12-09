ae0f66a111c5936971e0b050c17c932df32916df
anne-sop <anne-sophie.bach@outlook.com>
2014-12-09 10:54:52 +0100

    category function

--- classes/Category.php
+++ classes/Category.php
@@ -12,34 +12,50 @@ class Category {
 	}
 
 
-	public setId($id){
+	public function setId($id){
 	 	$this->id = $id;
 	}
 
-	public setName($name){
+	public function setName($name){
 		$this->name = $name;
 	}
 
-	public setPosition($position){
+	public function setPosition($position){
 		$this->position = $position;
 	}
 
-	public getId(){
+	public function getId(){
 		return $this->id;
 	}
 
-	public getName(){
+	public function getName(){
 		return $this->name;
 	}
 
-	public getPosition(){
+	public function getPosition(){
 		return $this->position;
 	}
 
 
-	public getAllTasks(){
-		
+	public function getAllTasks($id){
+		$query = "SELECT * FROM task WHERE id = '$id'";
 
+	$sth = $dbh->prepare($query);
+	$reponse = $sth->execute();
+	$tasks =array();
+	$i=0;
+
+	while ($reponse = $sth->fetch()) {
+   	 	$tasks[$i]['id'] = $reponse['id'];
+   		$tasks[$i]['category'] = $reponse['id_category'];
+   		$tasks[$i]['content'] = $reponse['content'];
+    	$tasks[$i]['date_end'] = $reponse['date_end'];
+    	$tasks[$i]['end_type'] = $reponse['end_type'];
+    	$tasks[$i]['status'] = $reponse['status'];
+    	$i++;
+    	$task = new Task($tasks[$i]);
+		}	
+	return $task;
 	}
 
 	
