8cc9043bc0f303817cb1e5d97b23c7ef41518c4c
Antoine92 <antoine.hagues@gmail.com>
2014-12-09 15:57:35 +0100

    Ajout des taches dans la base (return vide)

--- classes/Tools.php
+++ classes/Tools.php
@@ -2,7 +2,19 @@
 
 class Tools {
 
-	public function addTask($id_category = null, $content = null, $date_end = null, $end_type = null){
+	public static function addTask($id_category = null, $content = null, $date_end = null, $end_type = null){
+		global $dbh;
+		if($date_end=="")
+		{
+			$date_end=date("Y-m-d H:i:s");
+		}
+		$req=$dbh->prepare("INSERT INTO `task` (`id_category`, `content`, `date_end`, `end_type`, `status`) VALUES (:category, :content, :date, :end_type, '0')");
+		$req->execute(array(
+				":category"=>$id_category,
+				":content"=>$content,
+				":date"=>$date_end,
+				":end_type"=>$end_type
+			));
 	}
 	public function addCategory($name = null, $position = null){
 		
