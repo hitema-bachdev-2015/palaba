ace165f21e1307fd004fe82aa43ed307c48ee4a4
gael <navecbatchi@gmail.com>
2014-12-09 09:50:10 +0100

    no message

--- includes/bdd.php
+++ includes/bdd.php
@@ -1,4 +1,4 @@
 <?php
-include "config.php";
-
-//new PDO("mysql:host...", MYSQL_USER, MYSQL_PASS);
+$bdd = new PDO('mysql:host=localhost;dbname=todolist', 'root', '', array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+																								PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+																								PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
