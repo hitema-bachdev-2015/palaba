0fac28ee07dc1a37650388689db6af50d4bbda7f
thomas <engelsthomas92@gmail.com>
2014-12-10 16:13:39 +0100

    Modification tache

---
+++ includes/edittask.php
@@ -0,0 +1,24 @@
+<div class="clear both"></div>
+<div class="add-cat">
+    <a class="fancybox add-cat" rel="nofollow" href="#">+</a>
+</div>
+
+<div id="titi" style="display:none;">
+	<h2>Modifier une tâche</h2><br>
+    <form method="post" action="" >
+    	<label>
+    		<span>Nom Tache :</span>
+    		<input type="text" name="add-task-name" class="nameTaskUp" placeholder="Modifier une tache">
+    	</label>
+    	<label>
+    		<span>Type de fin :</span>
+    		<input type="text" name="task-type" class="typeTask" placeholder="Tache de la tache">
+    	</label>
+    	<label>
+    		<span>Date de fin :</span>
+    		<input type="text" class="date_end" placeholder="Date" id="dpEdit" >
+
+    	</label>
+        <input type="submit" class="btnOkFormEdit" value="OK">
+    </form>
+</div>
