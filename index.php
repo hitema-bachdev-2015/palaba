887670ac5d0dea83e6e484e053ee1dc7090e7c9c
Kevin <contact@kevindesousa.fr>
2014-12-10 19:09:28 +0100

    [*] Mise en commentaire de l'erreur de confirm-delete.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 		<?php include('includes/content.php'); ?>
 		
 		<?php include('includes/add-category.php'); ?>
-		<?php include('includes/confirm-delete.php'); ?>
+		<?php /**include('includes/confirm-delete.php');*/ ?>
 	</div>
 	<?php include('includes/tasks-complete.php'); ?>
 	<?php include('includes/footer.php'); ?>
