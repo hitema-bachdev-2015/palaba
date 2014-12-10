7eae7e77e02dbfc17ef68b31223ac7bb5d393253
Antoine92 <antoine.hagues@gmail.com>
2014-12-10 09:54:13 +0100

    return tab JSON Ok

--- ajax/script-addtask.php
+++ ajax/script-addtask.php
@@ -6,12 +6,27 @@
 	$category=$_POST["CATEGORY"];
 	$date=$_POST["DATE"];
 	$end_date=$_POST["END_TYPE_TASK"];
-	Tools::addTask($category,$content,$date,$end_date);
+	$last_id=Tools::addTask($category,$content,$date,$end_date);
+
+	$query = "SELECT id FROM task ORDER BY end_type ASC,date_end ASC";
+	$sth = $dbh->prepare($query);
+	$sth->execute();
+	$rows=$sth->fetchAll();		
+	for($i=0;$i<count($rows);$i++)
+	{
+		if($rows[$i]["id"]==$last_id)
+		{
+			$last_id=$i;
+		}
+	}
+
 	$tab=array(
 			"content"=>$content,
 			"category"=>$category,
 			"date"=>$date,
-			"end_type_date"=>$end_date
+			"end_type_date"=>$end_date,
+			"last_id"=>$last_id
 		);
+
 		echo json_encode($tab); 
 ?>
