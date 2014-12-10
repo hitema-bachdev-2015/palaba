d7f41b30f3672f4d16a8e9858b45d96b81a3162f
matthieucros <matthieu.cros7@gmail.com>
2014-12-10 21:52:55 +0100

    maj

--- includes/tasks-complete.php
+++ includes/tasks-complete.php
@@ -1,10 +1,3 @@
-<form method="POST" action="" name="complete-task">
+<form method="POST" action="" class="complete-task">
 	<input type="submit" value="Marquer les tâches comme faites" name="btnTaskComplete">
 </form>
-<style>
-#task-complete{
-	width: 200px;
-	height: 30px;
-	border: 1px solid black;
-}
-</style>
