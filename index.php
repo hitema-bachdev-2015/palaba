9d4de92025042438a9a9a6c4f67c91920828efe6
Kevin Martin <kevin.martin@quatdev.fr>
2014-12-10 15:19:15 +0100

    Mise en local des ressources externes

--- index.php
+++ index.php
@@ -15,14 +15,13 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/jquery.datetimepicker.css">
-	<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+	<link rel="stylesheet" href="css/jquery-ui.css" />
 	<link rel="stylesheet" href="css/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
 	<link rel="stylesheet" href="css/fancybox/helpers/jquery.fancybox-buttons.css" type="text/css" media="screen" />
 	<link rel="stylesheet" href="css/fancybox/helpers/jquery.fancybox-thumbs.css" type="text/css" media="screen" />
 	<!-- JAVASCRIPT -->
-	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery-ui.min.js"></script>
 	<script type="text/javascript" src="js/jquery.datetimepicker.js"></script>
 	<script type="text/javascript" src="js/task.js"></script>
 	<script type="text/javascript" src="js/category.js"></script>
