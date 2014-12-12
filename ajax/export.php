df24eaa6cc850b6472d7a8f58a0e5e7d9312cb5e
navec <navecbatchi@gmail.com>
2014-12-12 01:48:39 +0100

    entete pour l'exportation csv

--- ajax/export.php
+++ ajax/export.php
@@ -3,16 +3,20 @@
 	require_once('../classes/Tools.php');
 
     $fp = fopen('../fichier/category.csv', 'w');
-
+    // Nom des colonnes
+    $column = array('id', 'name', 'position');
+	fputcsv($fp, $column, ';');
+	// Value
 	foreach (Tools::getListCat() as $cat) {
 		fputcsv($fp, $cat, ';');		
 	} 
-
 	fclose($fp);
 
 
     $fp = fopen('../fichier/task.csv', 'w');
-
+    // Nom des colonnes
+    $column = array('id', 'id_category', 'content', 'date_end', 'end_type', 'status');
+	fputcsv($fp, $column, ';');
 	foreach (Tools::getListTask() as $task) {
 		fputcsv($fp, $task, ';');		
 	} 
