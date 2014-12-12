c7e39d660d47e174de2fe7d3194be15927209b0c
Yannick <yannick.elusue@hotmail.fr>
2014-12-12 16:13:47 +0100

    Deleted tasks in both categories

--- includes/sameTask.php
+++ includes/sameTask.php
@@ -1,4 +1,8 @@
 <div id="error_same_task" style="display:none; text-align: center;">
+<<<<<<< Updated upstream
 	<h2>You have already added this Task !<br/></h2><br><br>
+=======
+	<h2>You have already add this Task !<br/></h2><br><br>
+>>>>>>> Stashed changes
    	<button id="Ok" value="Ok" style="margin: auto;"/>OK</button>  		
 </div>
