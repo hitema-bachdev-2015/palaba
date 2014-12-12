ace20c62837521eed6e40e952b4ce59affba5101
thomas <engelsthomas92@gmail.com>
2014-12-12 10:16:06 +0100

    update task requete suite

--- ajax/updateTask.php
+++ ajax/updateTask.php
@@ -2,5 +2,5 @@
 require_once('../includes/bdd.php');
 require_once('../classes/Task.php');
 $myFunction = new Task($_POST['name']);
-$myFunction->updateTask();
+$myFunction->updateTask($_POST['content'],$_POST['end_type'],$_POST['date_end']);
 ?>
