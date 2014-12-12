78025d66f08ad80d59b8b261b71748832efa0a17
thomas <engelsthomas92@gmail.com>
2014-12-12 16:56:11 +0100

    traduction

--- includes/edittask.php
+++ includes/edittask.php
@@ -4,14 +4,14 @@
 </div>
 
 <div id="titi" style="display:none;">
-	<h2>Modifier une tâche</h2><br>
+	<h2>Edit task </h2><br>
     <form method="post" action="" >
     	<label>
-    		<span>Nom Tache :</span>
+    		<span>Task Name:</span>
     		<input type="text" name="add-task-name" class="nameTaskUp" placeholder="Modifier une tache">
     	</label>
     	<label>
-    		<span>Type de fin :</span>
+    		<span>End Type :</span>
                 <select name="task-type" class="typeTask">
             <option value="0">Au plus tot</option>
             <option value="1">Date precise</option>
@@ -20,7 +20,7 @@
     		<!-- <input type="text" name="task-type" class="typeTask" placeholder="Tache de la tache"> -->
     	</label>
     	<label>
-    		<span>Date de fin :</span>
+    		<span>End Date:</span>
     		<input type="text" class="date_end" placeholder="Date" id="dpEdit" >
 
     	</label>
